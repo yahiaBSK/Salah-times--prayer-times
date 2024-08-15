22634e81e53b442c15e9d6fc13c070789692ffab
yahiaBSK <boussouakyahia@gmail.com>
2024-08-15 07:12:50 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useRef } from 'react'
 import MainPage from './components/MainPage.jsx'
 
 function App() {
+  const pageTitle = document.title
+  const intervalId = useRef(null)
+  useEffect(()=>{
+    updatePageTitle()
+  },[])
+  function updatePageTitle(){
+    setInterval(()=>{
+      document.title = "Prayer times"
+      console.log("hi there")
+    }, 5000)
+    setInterval(()=>{
+      document.title = pageTitle
+      console.log("hi there")
+    }, 10000)
+  }
 
   return (
     <div className='mainBody'>
