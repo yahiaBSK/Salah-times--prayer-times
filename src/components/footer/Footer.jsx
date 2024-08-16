5b66a6812dd7a5785473f84bd00bb0a0470c2b10
yahiaBSK <boussouakyahia@gmail.com>
2024-08-16 18:31:19 +0100

    add responsive style (phones) - footer + prayercards

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,10 +19,10 @@ function Footer() {
         
       </div>
       <div className="socialLinksDiv">
-        <a href="https://www.facebook.com/BSK.yahia/" target='_blank' id="facebook"><i class="fa-brands fa-facebook-f"></i></a>
-        <a href="https://www.instagram.com/bsk.yahia/" target='_blank' id="instagram"><i class="fa-brands fa-instagram"></i></a>
-        <a href="https://github.com/yahiaBSK" target='_blank' id="github"><i class="fa-brands fa-github-alt"></i></a>
-        <a href="https://www.linkedin.com/in/yahia-bsk/" target='_blank' id="linkedin"><i class="fa-brands fa-linkedin-in"></i></a>
+        <a href="https://www.facebook.com/BSK.yahia/" target='_blank' id="facebook"><i className="fa-brands fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com/bsk.yahia/" target='_blank' id="instagram"><i className="fa-brands fa-instagram"></i></a>
+        <a href="https://github.com/yahiaBSK" target='_blank' id="github"><i className="fa-brands fa-github-alt"></i></a>
+        <a href="https://www.linkedin.com/in/yahia-bsk/" target='_blank' id="linkedin"><i className="fa-brands fa-linkedin-in"></i></a>
       </div>
     </div>
     </>
